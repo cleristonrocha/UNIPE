f3c3c1c3c3128b54cb30a4a02121e799d835bb7d
cleristonrocha <36806984+cleristonrocha@users.noreply.github.com>
2018-02-28 18:07:22 -0300

    Add files via upload

---
+++ Prefeitura.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Prefeitura {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	float salario;
+	float margem;
+	float prestacao;
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Salario: ");
+	salario = input.nextFloat();
+	
+	margem = salario*30/100;
+	
+	System.out.println("Valor da Prestacao: ");
+	prestacao = input.nextFloat();
+	
+	if(margem >= prestacao){
+		
+		System.out.println("Emprestimo concedido!");
+		
+	} else {
+
+		System.out.println("Emprestimo negado. Margem insuficiente!");
+	}
+	
+	input.close();
+	
+	}
+
+}
